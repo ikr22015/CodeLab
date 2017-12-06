d328e56b418d7bbbe527375cc8470898b17b94cf
Imran Kabir <ikr22015@gmail.com>
2017-12-06 15:29:53 -0500

    Create Codelab Imperative Programming project.

---
+++ imperativeProgramming/src/inputOutput/InputOutput1.java
@@ -0,0 +1,18 @@
+package inputOutput;
+
+import java.util.Scanner;
+
+public class InputOutput1 {
+
+	public static void main(String[] args) {
+		Scanner stdin = new Scanner(System.in);
+		
+		String name1 = stdin.next();
+		String name2 = stdin.next();
+		String name3 = stdin.next();
+		
+		System.out.println("\""+name1+", "+name2+", and "+name1+"\"");
+		System.out.println("\""+name2+", "+name1+", and "+name1+"\"");
+	}
+
+}
